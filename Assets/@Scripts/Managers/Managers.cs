d6cad9ac66497041f46e52ef1310d655f5da0c2e
stellarest <stellarest@gmail.com>
2024-03-08 04:35:45 +0900

    [Test]Commit try again by SourceTree

--- Assets/@Scripts/Managers/Managers.cs
+++ Assets/@Scripts/Managers/Managers.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-    // 한글 주석 테스트, 소스트리 커밋 준비
-    // Third Test
+    // Commit, Push Try Again by SourceTree
     void Start()
     {
         
